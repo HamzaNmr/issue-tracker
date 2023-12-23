62b7c0eb803aa5313d071b3a4bda39243063b9b2
Hamza Nemer <hamza.nmr.acc@gmail.com>
2023-12-23 09:33:42 +0200

    style: add active link feature

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,13 @@
+'use client'
+
 import Link from 'next/link'
 import React from 'react'
+import classnames from 'classnames'
+import { usePathname } from 'next/navigation'
 import { IoBugSharp } from "react-icons/io5"
 
 const NavBar = () => {
+    const currentPath = usePathname()
     const links = [
         { label: 'Dashboard', href: '/'},
         { label: 'Issues', href: '/issues'}
@@ -13,7 +18,13 @@ const NavBar = () => {
         <ul className='flex space-x-6'>
             {links?.map(link => 
                 <li key={link.label}>
-                    <Link href={link.href} className='text-zinc-500 hover:text-zinc-800 transitions-colors'>{link.label}</Link>
+                    <Link href={link.href} 
+                      className={classnames({
+                          'text-zinc-900' : link.href === currentPath,
+                          'text-zinc-500' : link.href !== currentPath,
+                          'hover:text-zinc-800 transitions-colors font-semibold' : true
+                      })}
+                    >{link.label}</Link>
                 </li>
             )}
         </ul>
